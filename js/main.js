35f6083bdf0239a7c419d742836991c8b42dd3ce
FrancescoMazzocchi <123@email.it>
2023-11-20 16:09:30 +0100

    feat: stampa email in pagina a comando

--- js/main.js
+++ js/main.js
@@ -3,9 +3,22 @@ const { createApp } = Vue;
 const object = {
     data: function () {
         return {
-
+            email: [],
+            contatore:0
         }
     },
+    methods: {
+        genera() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(r => {
+                if (r.data.success) {
+                    this.email.push(r.data.response);
+                }
+            })
+            this.contatore++
+        }
+
+    },
+
 };
 
 createApp(object).mount('#app')
